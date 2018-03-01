b6232f097f27e4b9eb7ebd6039fd985042cd488e
Antonio Vargas <juanantoniodamianv@gmail.com>
2018-03-01 08:07:35 -0300

    Component Swiper (slider) has been implemented. (with errors in the view).

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import List from './src/components/List';
+import Slide from './src/components/Slider';
 
 export default class App extends Component {
   render() {
     return (
       <View style={styles.container}>
+        <Slide />
         <List />
       </View>
     );
